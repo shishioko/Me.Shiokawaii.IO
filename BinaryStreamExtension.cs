24aaee64f0322bcc575d5556ebe5fd3dc5c2f98c
shio <shishioko@proton.me>
2026-03-07 23:26:57 +0100

    added class for simpler faster serialization

---
+++ BinaryStreamExtension.cs
@@ -0,0 +1,463 @@
+using System;
+using System.Buffers.Binary;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Me.Shiokawaii.IO
+{
+    public static class BinaryStreamExtension
+    {
+        public static async Task<byte[]> ReadU8AAsync(this Stream stream, int size)
+        {
+            byte[] data = new byte[size];
+            int position = 0;
+            while (position < size)
+            {
+                int read = await stream.ReadAsync(data.AsMemory(position, size - position));
+                position += read;
+                if (read == 0) throw new EndOfStreamException();
+            }
+            return data;
+        }
+        public static byte[] ReadU8A(this Stream stream, int size)
+        {
+            byte[] data = new byte[size];
+            int position = 0;
+            while (position < size)
+            {
+                int read = stream.Read(data, position, size - position);
+                position += read;
+                if (read == 0) throw new EndOfStreamException();
+            }
+            return data;
+        }
+        public static async Task WriteU8AAsync(this Stream stream, byte[] data)
+        {
+            await stream.WriteAsync(data);
+        }
+        public static void WriteU8A(this Stream stream, byte[] data)
+        {
+            stream.Write(data);
+        }
+        
+        public static async Task<byte> ReadU8Async(this Stream stream)
+        {
+            return (await stream.ReadU8AAsync(1))[0];
+        }
+        public static byte ReadU8(this Stream stream)
+        {
+            return stream.ReadU8A(1)[0];
+        }
+        public static async Task WriteU8Async(this Stream stream, byte data)
+        {
+            await stream.WriteAsync(new byte[] { data });
+        }
+        public static void WriteU8(this Stream stream, byte data)
+        {
+            stream.Write([data]);
+        }
+        
+        public static async Task<sbyte> ReadS8Async(this Stream stream)
+        {
+            return (sbyte)(await stream.ReadU8AAsync(1))[0];
+        }
+        public static sbyte ReadS8(this Stream stream)
+        {
+            return (sbyte)(stream.ReadU8A(1))[0];
+        }
+        public static async Task WriteS8Async(this Stream stream, sbyte data)
+        {
+            await stream.WriteAsync(MemoryMarshal.AsBytes<sbyte>(new sbyte[] { data }).ToArray());
+        }
+        public static void WriteS8(this Stream stream, sbyte data)
+        {
+            stream.Write(MemoryMarshal.AsBytes<sbyte>(new sbyte[] { data }).ToArray());
+        }
+        
+        public static async Task<ushort> ReadU16Async(this Stream stream)
+        {
+            byte[] buffer = await stream.ReadU8AAsync(sizeof(ushort));
+            return BinaryPrimitives.ReadUInt16BigEndian(buffer);
+        }
+        public static ushort ReadU16(this Stream stream)
+        {
+            byte[] buffer = stream.ReadU8A(sizeof(ushort));
+            return BinaryPrimitives.ReadUInt16BigEndian(buffer);
+        }
+        public static async Task WriteU16Async(this Stream stream, ushort data)
+        {
+            byte[] buffer = new byte[sizeof(ushort)];
+            BinaryPrimitives.WriteUInt16BigEndian(buffer, data);
+            await stream.WriteAsync(buffer);
+        }
+        public static void WriteU16(this Stream stream, ushort data)
+        {
+            byte[] buffer = new byte[sizeof(ushort)];
+            BinaryPrimitives.WriteUInt16BigEndian(buffer, data);
+            stream.Write(buffer);
+        }
+        
+        public static async Task<short> ReadS16Async(this Stream stream)
+        {
+            byte[] buffer = await stream.ReadU8AAsync(sizeof(short));
+            return BinaryPrimitives.ReadInt16BigEndian(buffer);
+        }
+        public static short ReadS16(this Stream stream)
+        {
+            byte[] buffer = stream.ReadU8A(sizeof(short));
+            return BinaryPrimitives.ReadInt16BigEndian(buffer);
+        }
+        public static async Task WriteS16Async(this Stream stream, short data)
+        {
+            byte[] buffer = new byte[sizeof(short)];
+            BinaryPrimitives.WriteInt16BigEndian(buffer, data);
+            await stream.WriteAsync(buffer);
+        }
+        public static void WriteS16(this Stream stream, short data)
+        {
+            byte[] buffer = new byte[sizeof(short)];
+            BinaryPrimitives.WriteInt16BigEndian(buffer, data);
+            stream.Write(buffer);
+        }
+        
+        public static async Task<uint> ReadU32Async(this Stream stream)
+        {
+            byte[] buffer = await stream.ReadU8AAsync(sizeof(uint));
+            return BinaryPrimitives.ReadUInt32BigEndian(buffer);
+        }
+        public static uint ReadU32(this Stream stream)
+        {
+            byte[] buffer = stream.ReadU8A(sizeof(uint));
+            return BinaryPrimitives.ReadUInt32BigEndian(buffer);
+        }
+        public static async Task WriteU32Async(this Stream stream, uint data)
+        {
+            byte[] buffer = new byte[sizeof(uint)];
+            BinaryPrimitives.WriteUInt32BigEndian(buffer, data);
+            await stream.WriteAsync(buffer);
+        }
+        public static void WriteU32(this Stream stream, uint data)
+        {
+            byte[] buffer = new byte[sizeof(uint)];
+            BinaryPrimitives.WriteUInt32BigEndian(buffer, data);
+            stream.Write(buffer);
+        }
+        
+        public static async Task<int> ReadS32Async(this Stream stream)
+        {
+            byte[] buffer = await stream.ReadU8AAsync(sizeof(int));
+            return BinaryPrimitives.ReadInt32BigEndian(buffer);
+        }
+        public static int ReadS32(this Stream stream)
+        {
+            byte[] buffer = stream.ReadU8A(sizeof(int));
+            return BinaryPrimitives.ReadInt32BigEndian(buffer);
+        }
+        public static async Task WriteS32Async(this Stream stream, int data)
+        {
+            byte[] buffer = new byte[sizeof(int)];
+            BinaryPrimitives.WriteInt32BigEndian(buffer, data);
+            await stream.WriteAsync(buffer);
+        }
+        public static void WriteS32(this Stream stream, int data)
+        {
+            byte[] buffer = new byte[sizeof(int)];
+            BinaryPrimitives.WriteInt32BigEndian(buffer, data);
+            stream.Write(buffer);
+        }
+        
+        public static async Task<ulong> ReadU64Async(this Stream stream)
+        {
+            byte[] buffer = await stream.ReadU8AAsync(sizeof(ulong));
+            return BinaryPrimitives.ReadUInt64BigEndian(buffer);
+        }
+        public static ulong ReadU64(this Stream stream)
+        {
+            byte[] buffer = stream.ReadU8A(sizeof(ulong));
+            return BinaryPrimitives.ReadUInt64BigEndian(buffer);
+        }
+        public static async Task WriteU64Async(this Stream stream, ulong data)
+        {
+            byte[] buffer = new byte[sizeof(ulong)];
+            BinaryPrimitives.WriteUInt64BigEndian(buffer, data);
+            await stream.WriteAsync(buffer);
+        }
+        public static void WriteU64(this Stream stream, ulong data)
+        {
+            byte[] buffer = new byte[sizeof(ulong)];
+            BinaryPrimitives.WriteUInt64BigEndian(buffer, data);
+            stream.Write(buffer);
+        }
+        
+        public static async Task<long> ReadS64Async(this Stream stream)
+        {
+            byte[] buffer = await stream.ReadU8AAsync(sizeof(long));
+            return BinaryPrimitives.ReadInt64BigEndian(buffer);
+        }
+        public static long ReadS64(this Stream stream)
+        {
+            byte[] buffer = stream.ReadU8A(sizeof(long));
+            return BinaryPrimitives.ReadInt64BigEndian(buffer);
+        }
+        public static async Task WriteS64Async(this Stream stream, long data)
+        {
+            byte[] buffer = new byte[sizeof(long)];
+            BinaryPrimitives.WriteInt64BigEndian(buffer, data);
+            await stream.WriteAsync(buffer);
+        }
+        public static void WriteS64(this Stream stream, long data)
+        {
+            byte[] buffer = new byte[sizeof(long)];
+            BinaryPrimitives.WriteInt64BigEndian(buffer, data);
+            stream.Write(buffer);
+        }
+        
+        public static async Task<float> ReadF32Async(this Stream stream)
+        {
+            byte[] buffer = await stream.ReadU8AAsync(sizeof(float));
+            return BinaryPrimitives.ReadSingleBigEndian(buffer);
+        }
+        public static float ReadF32(this Stream stream)
+        {
+            byte[] buffer = stream.ReadU8A(sizeof(float));
+            return BinaryPrimitives.ReadSingleBigEndian(buffer);
+        }
+        public static async Task WriteF32Async(this Stream stream, float data)
+        {
+            byte[] buffer = new byte[sizeof(float)];
+            BinaryPrimitives.WriteSingleBigEndian(buffer, data);
+            await stream.WriteAsync(buffer);
+        }
+        public static void WriteF32(this Stream stream, float data)
+        {
+            byte[] buffer = new byte[sizeof(float)];
+            BinaryPrimitives.WriteSingleBigEndian(buffer, data);
+            stream.Write(buffer);
+        }
+        
+        public static async Task<double> ReadF64Async(this Stream stream)
+        {
+            byte[] buffer = await stream.ReadU8AAsync(sizeof(double));
+            return BinaryPrimitives.ReadDoubleBigEndian(buffer);
+        }
+        public static double ReadF64(this Stream stream)
+        {
+            byte[] buffer = stream.ReadU8A(sizeof(double));
+            return BinaryPrimitives.ReadDoubleBigEndian(buffer);
+        }
+        public static async Task WriteF64Async(this Stream stream, double data)
+        {
+            byte[] buffer = new byte[sizeof(double)];
+            BinaryPrimitives.WriteDoubleBigEndian(buffer, data);
+            await stream.WriteAsync(buffer);
+        }
+        public static void WriteF64(this Stream stream, double data)
+        {
+            byte[] buffer = new byte[sizeof(double)];
+            BinaryPrimitives.WriteDoubleBigEndian(buffer, data);
+            stream.Write(buffer);
+        }
+        
+        public static async Task<uint> ReadU32VAsync(this Stream stream)
+        {
+            uint data = 0;
+            int position = 0;
+            while (true)
+            {
+                byte current = await stream.ReadU8Async();
+                data |= (current & 127U) << position;
+                if ((current & 128) == 0) return data;
+                position += 7;
+                if (position >= sizeof(uint) * 8) throw new ProtocolViolationException();
+            }
+        }
+        public static uint ReadU32V(this Stream stream)
+        {
+            uint data = 0;
+            int position = 0;
+            while (true)
+            {
+                byte current = stream.ReadU8();
+                data |= (current & 127U) << position;
+                if ((current & 128) == 0) return data;
+                position += 7;
+                if (position >= sizeof(uint) * 8) throw new ProtocolViolationException();
+            }
+        }
+        public static async Task<int> WriteU32VAsync(this Stream stream, uint data)
+        {
+            int size = 0;
+            do
+            {
+                byte current = (byte)data;
+                data >>= 7;
+                if (data != 0) current |= 128;
+                else current &= 127;
+                await stream.WriteU8Async(current);
+                size++;
+            }
+            while (data != 0);
+            return size;
+        }
+        public static int WriteU32V(this Stream stream, uint data)
+        {
+            int size = 0;
+            do
+            {
+                byte current = (byte)data;
+                data >>= 7;
+                if (data != 0) current |= 128;
+                else current &= 127;
+                stream.WriteU8(current);
+                size++;
+            }
+            while (data != 0);
+            return size;
+        }
+        
+        public static async Task<int> ReadS32VAsync(this Stream stream)
+        {
+            return (int)await stream.ReadU32VAsync();
+        }
+        public static int ReadS32V(this Stream stream)
+        {
+            return (int)stream.ReadU32V();
+        }
+        public static async Task<int> WriteS32VAsync(this Stream stream, int data)
+        {
+            return await stream.WriteU32VAsync((uint)data);
+        }
+        public static int WriteS32V(this Stream stream, int data)
+        {
+            return stream.WriteU32V((uint)data);
+        }
+        
+        public static async Task<ulong> ReadU64VAsync(this Stream stream)
+        {
+            ulong data = 0;
+            int position = 0;
+            while (true)
+            {
+                byte current = await stream.ReadU8Async();
+                data |= (current & 127U) << position;
+                if ((current & 128) == 0) return data;
+                position += 7;
+                if (position >= 64) throw new ProtocolViolationException();
+            }
+        }
+        public static ulong ReadU64V(this Stream stream)
+        {
+            ulong data = 0;
+            int position = 0;
+            while (true)
+            {
+                byte current = stream.ReadU8();
+                data |= (current & 127U) << position;
+                if ((current & 128) == 0) return data;
+                position += 7;
+                if (position >= 64) throw new ProtocolViolationException();
+            }
+        }
+        public static async Task<int> WriteU64VAsync(this Stream stream, ulong data)
+        {
+            int size = 0;
+            do
+            {
+                byte current = (byte)data;
+                data >>= 7;
+                if (data != 0) current |= 128;
+                else current &= 127;
+                await stream.WriteU8Async(current);
+                size++;
+            }
+            while (data != 0);
+            return size;
+        }
+        public static int WriteU64V(this Stream stream, ulong data)
+        {
+            int size = 0;
+            do
+            {
+                byte current = (byte)data;
+                data >>= 7;
+                if (data != 0) current |= 128;
+                else current &= 127;
+                stream.WriteU8(current);
+                size++;
+            }
+            while (data != 0);
+            return size;
+        }
+        
+        public static async Task<long> ReadS64VAsync(this Stream stream)
+        {
+            return (long)await stream.ReadU64VAsync();
+        }
+        public static long ReadS64V(this Stream stream)
+        {
+            return (long)stream.ReadU64V();
+        }
+        public static async Task<int> WriteS64VAsync(this Stream stream, long data)
+        {
+            return await stream.WriteU64VAsync((ulong)data);
+        }
+        public static int WriteS64V(this Stream stream, long data)
+        {
+            return stream.WriteU64V((ulong)data);
+        }
+        
+        public static async Task<bool> ReadBoolAsync(this Stream stream)
+        {
+            return (await stream.ReadU8Async()) != 0;
+        }
+        public static bool ReadBool(this Stream stream)
+        {
+            return (stream.ReadU8()) != 0;
+        }
+        public static async Task WriteBoolAsync(this Stream stream, bool data)
+        {
+            await stream.WriteAsync(MemoryMarshal.AsBytes<bool>(new bool[] { data }).ToArray());
+        }
+        public static void WriteBool(this Stream stream, bool data)
+        {
+            stream.Write(MemoryMarshal.AsBytes<bool>(new bool[] { data }).ToArray());
+        }
+        
+        public static async Task<Guid> ReadGuidAsync(this Stream stream)
+        {
+            byte[] buffer = await stream.ReadU8AAsync(16);
+            if (BitConverter.IsLittleEndian) buffer = [buffer[3], buffer[2], buffer[1], buffer[0], buffer[5], buffer[4], buffer[7], buffer[6], buffer[8], buffer[9], buffer[10], buffer[11], buffer[12], buffer[13], buffer[14], buffer[15]];
+            return MemoryMarshal.Cast<byte, Guid>(buffer)[0];
+        }
+        public static async Task WriteGuidAsync(this Stream stream, Guid data)
+        {
+            byte[] buffer = MemoryMarshal.AsBytes([data]).ToArray();
+            if (!BitConverter.IsLittleEndian)
+            {
+                await stream.WriteAsync(buffer);
+                return;
+            }
+            await stream.WriteAsync(new byte[] { buffer[3], buffer[2], buffer[1], buffer[0], buffer[5], buffer[4], buffer[7], buffer[6], buffer[8], buffer[9], buffer[10], buffer[11], buffer[12], buffer[13], buffer[14], buffer[15] });
+        }
+        public static Guid ReadGuid(this Stream stream)
+        {
+            byte[] buffer = stream.ReadU8A(16);
+            if (BitConverter.IsLittleEndian) buffer = [buffer[3], buffer[2], buffer[1], buffer[0], buffer[5], buffer[4], buffer[7], buffer[6], buffer[8], buffer[9], buffer[10], buffer[11], buffer[12], buffer[13], buffer[14], buffer[15]];
+            return MemoryMarshal.Cast<byte, Guid>(buffer)[0];
+        }
+        public static void WriteGuid(this Stream stream, Guid data)
+        {
+            byte[] buffer = MemoryMarshal.AsBytes([data]).ToArray();
+            if (!BitConverter.IsLittleEndian)
+            {
+                stream.Write(buffer);
+                return;
+            }
+            stream.Write([buffer[3], buffer[2], buffer[1], buffer[0], buffer[5], buffer[4], buffer[7], buffer[6], buffer[8], buffer[9], buffer[10], buffer[11], buffer[12], buffer[13], buffer[14], buffer[15]]);
+        }
+    }
+}
